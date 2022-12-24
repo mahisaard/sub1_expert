9e304c1865cb077694452d89d3f05b74a529d97c
Mahisa Ardana Wijaya <mahisaardanaw@gmail.com>
2022-12-24 09:30:44 +0700

    fixing crash test to display in firebase

--- module_movies/lib/presentation/pages/home_movie_page.dart
+++ module_movies/lib/presentation/pages/home_movie_page.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
