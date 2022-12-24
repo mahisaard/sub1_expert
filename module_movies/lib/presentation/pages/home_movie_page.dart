877978a6db34d94993112d783917595c4383e121
Mahisa Ardana Wijaya <mahisaardanaw@gmail.com>
2022-12-24 09:37:57 +0700

    fixing error with google service

--- module_movies/lib/presentation/pages/home_movie_page.dart
+++ module_movies/lib/presentation/pages/home_movie_page.dart
@@ -1,6 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:module_common/common/constants.dart';
